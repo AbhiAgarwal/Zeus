ee3c1a01f07ad38c4802c4b40c74ba752e6d600f
Abhi Agarwal <me@abhiagarwal.com>
2013-06-30 14:09:17 +0700

    Update mind_server.js

--- mind_server.js
+++ mind_server.js
@@ -2,12 +2,11 @@ var nodeThinkGear = require('node-thinkgear'),
 say = require('say'),
 translate = require('translate'),
 sys = require('sys'),
-colors = require('colors'),
 appjs = require('appjs');
 
 var tgClient = nodeThinkGear.createClient({
-	appName:'AbhiAgarwal',
-	appKey:'0fc4141b4b45c675cc8d3a765b8d71c5bde9390'
+	appName:'',
+	appKey:''
 });
 
 tgClient.on('data', function(data){
